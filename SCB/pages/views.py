d8433af77894d0906f66d687af682ee6bb611091
JIOjosBG <jjjooosssiiifff@gmail.com>
2020-10-02 14:33:41 +0300

    articles

--- SCB/pages/views.py
+++ SCB/pages/views.py
@@ -4,5 +4,23 @@ from django.shortcuts import render
 
 def home(request):
     context = {
+        "articles":[
+            {
+                "zaglavie":"novini dnes",
+                "tqlo":"dnes v 19:00....",
+                "date_of_action":"19:00,01.10.20",
+                "date_posted":"23:00,01.10.20",
+                "city":"Sofia",
+                "clicks":0,
+            },
+            {
+                "zaglavie":"novini utre",
+                "tqlo":"utre v 12:00....",
+                "date_of_action":"12:00,02.10.20",
+                "date_posted":"23:00,01.10.20",
+                "city":"Sofia",
+                "clicks":0,
+            }
+        ]
     }
-    return render(request, 'pages/home.html', context)
+    return render(request, 'pages/news.html', context)
