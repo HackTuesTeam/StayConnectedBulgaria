af72ac49b2859702cc3ef49184bbb4b676a980aa
JIOjosBG <jjjooosssiiifff@gmail.com>
2020-10-02 13:06:18 +0300

    new commit'

--- SCB/users/views.py
+++ SCB/users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def user_list(request):
+        context = {
+        }
+        return render(request, 'users/user_list.html', context)
